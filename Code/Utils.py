86d89e03831740443d00cc674fc74099544652e8
alifthi <alifathi8008@gmail.com>
2023-04-01 20:04:21 +0430

    NeuralMachineTranslation: loadData method developed to loading data from .txt file

---
+++ Code/Utils.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import pandas as pd
+import string
+import re
+class Data:
+    def __init__(self,path) -> None:
+        self.path = path
+    def loadData(self):
+        self.data = pd.read_csv(self.path,sep='\t')
+        self.data.columns = ['eng','sp']
+    def preprocessing(self):
+        pass
