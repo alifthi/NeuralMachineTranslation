d095da0d7e5a9949658eb0af3aabb91e8908f7a9
alifthi <alifathi8008@gmail.com>
2023-04-01 20:21:38 +0430

    NeuralMachineTranslation: dataProperties method developed to describe data length

--- Code/Utils.py
+++ Code/Utils.py
@@ -8,7 +8,24 @@ class Data:
     def loadData(self):
         self.data = pd.read_csv(self.path,sep='\t')
         self.data.columns = ['eng','sp']
+    def dataProperties(self):
+        dataWordCount = []
+        for j in ['eng','sp']:
+            lenOfWords = []
+            for i in self.data[j]:
+                lenOfWords.append(len(i.split(' ')))
+            dataWordCount.append(lenOfWords)
+        lenOfWords = pd.DataFrame(dataWordCount)
+        lenOfWords = lenOfWords.T
+        lenOfWords.columns = ['eng','sp']
+        print(lenOfWords.describe())
+
     def preprocessing(self):
         spanish = self.data['sp']
         eng = self.data['eng']
         spanish = '[Start] ' + spanish+ ' [End]'
+data = Data(r"C:\Users\alifa\Documents\AI\robotech\transformers\docs\w4\nmt example\nmt example\spa.txt")
+data.loadData()
+data.dataProperties()
+data.preprocessing()
+           
