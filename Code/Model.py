9fad4378828f5e707e17100de8dee31f90332ab3
alifthi <alifathi8008@gmail.com>
2023-04-02 16:41:53 +0430

    NeuralMachineTranslation: buildModel method developed

--- Code/Model.py
+++ Code/Model.py
@@ -1,6 +1,19 @@
 import tensorflow as tf
+from tensorflow.keras import layers as ksl
 class Model:
-    def __init__(self) -> None:
-        pass
-    def buildModel(slef):
-        pass
+    def __init__(self,numEnglishTokens,numSpanishTokens) -> None:
+        self.numEnglishTokens = numEnglishTokens
+        self.numSpanishTokens = numSpanishTokens
+    def buildModel(self):
+        engInp = ksl.Input([None])
+        x = ksl.Embedding(input_dim = self.numEnglishTokens,output_dim = 128)(engInp)
+        engGru = ksl.GRU(64)(x)
+        spInp = ksl.Input(self.numSpanishTokens)
+        x = ksl.Embedding(input_dim = self.numSpanishTokens,output_dim = 128)(spInp)
+        x = ksl.GRU(64)(x,initial_state = [engGru])
+        x = ksl.Dense(self.numSpanishTokens,'softmax')(x)
+        self.net = tf.keras.Model(inputs = [engInp,spInp],outputs = x)
+model = Model(100,150)
+model.buildModel()
+
+        
