8e028e8d605478a7662033c99a639119acdd4782
alifthi <alifathi8008@gmail.com>
2023-04-02 16:55:46 +0430

    NeuralMachineTranslation: adding TimeDistribution layer and developing compile method

--- Code/Model.py
+++ Code/Model.py
@@ -10,12 +10,16 @@ class Model:
         engGru = ksl.GRU(64)(x)
         spInp = ksl.Input(self.numSpanishTokens)
         x = ksl.Embedding(input_dim = self.numSpanishTokens,output_dim = 128)(spInp)
-        x = ksl.GRU(64)(x,initial_state = [engGru])
-        x = ksl.Dense(self.numSpanishTokens,'softmax')(x)
+        x = ksl.GRU(64,return_sequences = True)(x,initial_state = [engGru])
+        x = ksl.TimeDistributed(ksl.Dense(self.numSpanishTokens,'softmax'))(x)
         self.net = tf.keras.Model(inputs = [engInp,spInp],outputs = x)
     def compileModel(self):
-        pass
+        Adam = tf.keras.optimizers.Adam(learning_rate=0.01)
+        self.net.compile(
+            optimizer=Adam,
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"])
 model = Model(100,150)
 model.buildModel()
-
+model.compileModel()
         
