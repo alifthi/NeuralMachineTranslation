5d9c59fa03f10fa4373bdbd4370f0e3ff84b7eac
alifthi <alifathi8008@gmail.com>
2023-04-05 19:49:05 +0430

    NeuralMachineTranslation: inferences setup

---
+++ Code/inference.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import numpy as np
+
+modelPath = r"C:\Users\alifa\Documents\AI\Git\english-spnaish Translator\Model\model.h5"
+model = tf.keras.models.load_model(modelPath)
+model.summary()
+encoderInput = tf.keras.layers.Input([None])
+encoderEmbedding = model.layers[2](encoderInput)
+encoderLstm = model.layers[4](encoderEmbedding)
+encoderModel = tf.keras.Model(encoderInput,encoderLstm)
+
+decoderInput = tf.keras.layers.Input([None])
+decoderCState = tf.keras.layers.Input([None])
+decoderHState = tf.keras.layers.Input([None])
+decoderEmbedding = model.layers[3](decoderInput)
+decoderOutput,Hstate,Cstate = model.layers[5](decoderEmbedding,initial_state =[decoderCState,decoderHState])
+
+decoderDense = model.layers[6](decoderOutput)
+decoderModel = tf.keras.Model([decoderInput,decoderCState,decoderHState],decoderDense)
+
+def inference(inputSentence,vectorizer):
+    inputVector = vectorizer(inputSentence)
+    state = encoderModel.predict(inputVector)
+    decoderResponse = []
+    stopCondition = False
+    target = np.expand_dims([41],axis=0)   # 41 is index of [Start] special token
+    while not stopCondition:
+        output,HState,CState = decoderModel.predict([target]+state)
+        predictedWord = np.argmax(output)
+        decoderResponse.append(predictedWord)
+        inputVector = np.argmax(predictedWord,axis = 0)
+        state = [HState,CState]
