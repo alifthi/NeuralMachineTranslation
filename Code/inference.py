04c0b553ef4e941e2d651c326dde581eab4671a2
alifthi <alifathi8008@gmail.com>
2023-04-06 20:19:55 +0430

    NeuralMachineTranslation: debuging some bugs in geting input to the model.

--- Code/inference.py
+++ Code/inference.py
@@ -16,19 +16,19 @@ decoderEmbedding = model.layers[3](decoderInput)
 decoderOutput,Hstate,Cstate = model.layers[5](decoderEmbedding,initial_state =[decoderCState,decoderHState])
 
 decoderDense = model.layers[6](decoderOutput)
-decoderModel = tf.keras.Model([decoderInput,decoderCState,decoderHState],decoderDense)
+decoderModel = tf.keras.Model([decoderInput,decoderCState,decoderHState],[decoderDense,Hstate,Cstate])
 def inference(inputSentence,vectorizer,decoderOutputLen):
     inputVector = vectorizer(inputSentence)
-    state = encoderModel.predict(inputVector)
+    inputVector = np.expand_dims(inputVector,axis=0)   # 41 is index of [Start] special token
+    state = encoderModel.predict(inputVector)[1:]
     decoderResponse = []
     target = np.expand_dims([41],axis=0)   # 41 is index of [Start] special token
     while True:    
         output,HState,CState = decoderModel.predict([target]+state)
-        predictedWord = np.argmax(output)
-        decoderResponse.append(predictedWord)
-        target = np.argmax(predictedWord,axis = 0)
+        target = np.argmax(output)
+        decoderResponse.append(target)
+        target = np.expand_dims([target],axis = 0)
         state = [HState,CState]
         if decoderOutputLen < len(decoderResponse) or target == 3:
             break
     return decoderResponse
-        
